c3bfd6b9408248d9afe87613e196b21dfcb1d985
Andres Ehrenpreis <andres.ehrenpreis@gmail.com>
2021-11-17 15:18:24 +0200

    WIP: Change tam.registriisik service version 1->2

--- lib/x_road/services/tam.rb
+++ lib/x_road/services/tam.rb
@@ -7,8 +7,14 @@ module XRoad
         'GOV/70008799/tam'
       end
 
+      # NS should be: 'http://rr.x-road.eu/producer' as V6 standard suggest, but
+      # according to Ehis letter at 13.12.2017, ehis uses old one, thus we overwrite producer ns
+      # def producer_ns
+      #   'http://producers.tam.xtee.riik.ee/producer/tam'
+      # end
+
       def registriisik(personal_code)
-        service_path = producer_path + '/registriisik/v1'
+        service_path = producer_path + '/registriisik/v2'
         body = {
           isikukood: personal_code
         }
